7832a53955ea0b55e95806825c8a3b59510892ab
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-20 11:04:05 +0200

    added model and viewing model

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home_index'),
     path('bidule', views.bidule, name='home_bidule'),
-    path('messages', views.all_messages, name='all_messages')
+    path('messages', views.all_messages, name='all_messages'),
+    path('messages/<str:id>', views.show_message, name='show_message')
 
 ]
 
--- website/views.py
+++ website/views.py
@@ -19,8 +19,18 @@ def bidule(request):
 
     return render(request, 'website/bidule.html', {"bidule": "un nouveau message"})
 
+
 def all_messages(request):
     messages = Message.objects.all()
     context = {"messages":messages}
 
     return render(request, 'website/all.html', context)
+
+
+def show_message(request, id):
+
+    message = Message.objects.get(id=id)
+
+    return render(request, 'website/show.html', {"message":message})
+
+
