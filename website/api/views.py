0372d1f7f5b5d8736c701ecbb5d97c929daf4f20
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-25 16:46:24 +0200

    added API

--- website/api/views.py
+++ website/api/views.py
@@ -1,16 +1,18 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from website.api.serialzers import MessageSerializer
 from website.models import Message
 
 @api_view(['GET'])
-
 def get_messages(request):
     messages = Message.objects.all()
     serialized_messages = MessageSerializer(messages, many=True)
 
     return Response(serialized_messages.data)
 
+@api_view(['GET'])
+def get_message(request, id):
+    message = Message.objects.get(id=id)
+    serialized_message = MessageSerializer(message, many=False)
 
-def get_message(request):
-    return None
+    return Response(serialized_message.data)
