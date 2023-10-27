32efe14173d3eed102ce4920d6429d7ac59d8c36
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-27 12:39:11 +0200

    added JWTs

--- website/api/urls.py
+++ website/api/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('messages', views.get_messages),
-    path('message/new', views.new_messages),
+    path('message/new', views.new_message),
     path('message/<str:id>', views.get_message),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
