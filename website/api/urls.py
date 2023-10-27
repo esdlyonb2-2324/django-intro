91ebd1ac1028f3f5e12791d63fdb70cd58746bfc
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-27 09:30:57 +0200

    added API

--- website/api/urls.py
+++ website/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('messages', views.get_messages),
+    path('message/new', views.new_messages),
     path('message/<str:id>', views.get_message)
 ]
--- website/api/views.py
+++ website/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from website.api.serialzers import MessageSerializer
-from website.models import Message
+from website.models import Message, User, Category
+
 
 @api_view(['GET'])
 def get_messages(request):
@@ -16,3 +17,16 @@ def get_message(request, id):
     serialized_message = MessageSerializer(message, many=False)
 
     return Response(serialized_message.data)
+
+@api_view(['POST'])
+def new_messages(request):
+    if request.method == 'POST':
+        newMessage = Message()
+        newMessage.author = User.objects.get(id=1)
+        newMessage.category = Category.objects.get(id=request.POST.get["category"])
+        message = MessageSerializer(instance=newMessage, data=request.data)
+
+        if message.is_valid():
+            message.save()
+            return Response(message.data)
+    return None
