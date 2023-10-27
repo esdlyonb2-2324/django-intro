435c54054fcadb0f0ac2881214b8ab673c2b3437
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-27 10:30:54 +0200

    added API

--- website/api/serializers.py
+++ website/api/serializers.py
@@ -1,10 +1,27 @@
 from rest_framework.serializers import ModelSerializer
 
-from website.models import Message
+from website.models import Message, Category, User, Response
 
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+class AuthorSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+class ResponseSerializer(ModelSerializer):
+    class Meta:
+        model = Response
+        fields = ['id' ,'content', 'author']
+    author = AuthorSerializer()
 class MessageSerializer(ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
        # depth = 1
 
+    category = CategorySerializer()
+    author = AuthorSerializer()
+    responses = ResponseSerializer(many=True)
